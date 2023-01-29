a50b047d7f286aafa01bdcee784353e42870fa98
mtstall <mtstallings95@gmail.com>
2023-01-29 11:35:33 -0500

    added variables for packages needed in application and array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,47 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter project tile:',
+            name: 'title',
+            default: 'Project Title',
+          },
+          {
+            type: 'input',
+            message: 'Enter project description:',
+            name: 'description',
+            default: 'Project Description',
+          },
+          {
+            type: 'input',
+            message: 'Installation instructions:',
+            name: 'installation',
+            default: 'Installation Instructions',
+          },
+          {
+              type: 'input',
+              message: 'Usage:',
+              name: 'usage',
+              default: 'Usage',
+            },
+            {
+              type: 'input',
+              message: 'Contribution guidelines:',
+              name: 'contributing',
+              default: 'Contribution guidelines',
+            },
+            {
+                type: 'input',
+                message: 'Test instructions:',
+                name: 'tests',
+                default: 'Test instructions',
+              },
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
